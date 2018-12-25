f55e383844cb3a3d272283df69289c0bffcd72df
duguangxing <1097943091@qq.com>
2018-12-25 14:25:18 +0800

    Add files via upload

---
+++ assignment2.3/0.py
@@ -0,0 +1,27 @@
+import scipy
+from PIL import Image
+from scipy import ndimage
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
+
+from Tensorflow_Tutorial import model
+
+## START CODE HERE ## (PUT YOUR IMAGE NAME)
+my_image = "one_1.jpg"
+## END CODE HERE ##
+
+# We preprocess your image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(64,64)).reshape((1, 64*64*3)).T
+my_image_prediction = predict(my_image, parameters)
+
+plt.imshow(image)
+print("Your algorithm predicts: y = " + str(np.squeeze(my_image_prediction)))
+
+
